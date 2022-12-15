d1bfdf1744b8b2c5a22795c30bc36cc8ff40a8a3
Artur Faria Vieira <artur.f.vieira.94@gmail.com>
2022-12-14 21:13:56 -0300

    first commit

---
+++ Prática - Linkedriven/Exercício02.sql
@@ -0,0 +1,1 @@
+SELECT testimonials.id, testimonials.message, t1.name AS "writer", t2.name AS "recipient" FROM testimonials JOIN users AS "t1"  ON testimonials."writerId" = t1.id JOIN users AS "t2" ON testimonials."recipientId" = t2.id;
